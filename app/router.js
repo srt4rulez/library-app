c95b69d86b83948b26d793c3c27816a4dfb5d713
Jake Dennison <srt4rulez@gmail.com>
2017-04-02 19:35:38 -0400

    Add edit/delete for libs

--- app/router.js
+++ app/router.js
@@ -19,7 +19,10 @@ Router.map(function() {
     });
 
     this.route('libraries', function() {
+
         this.route('new');
+        this.route('edit', { path: '/:library_id/edit' });
+
     });
 
 });
