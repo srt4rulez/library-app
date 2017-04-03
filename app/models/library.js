19f43202558af7918f9e0cee49fa77e0c0995e15
Jake Dennison <srt4rulez@gmail.com>
2017-04-02 20:25:38 -0400

    Add new components to replace duplicate code

--- app/models/library.js
+++ app/models/library.js
@@ -1,7 +1,12 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
+
     name:    DS.attr('string'),
     address: DS.attr('string'),
-    phone:   DS.attr('string')
+    phone:   DS.attr('string'),
+
+    isValid: Ember.computed.notEmpty('name'),
+
 });
