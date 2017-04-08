cb81330b9d5bd264cbae334c3240a7bfb8ce72c3
Jake Dennison <srt4rulez@gmail.com>
2017-04-08 14:15:56 -0400

    Implement seeder

--- app/models/library.js
+++ app/models/library.js
@@ -1,14 +1,29 @@
 import DS from 'ember-data';
 import Ember from 'ember';
+import Faker from 'faker';
 
 export default DS.Model.extend({
 
     name:    DS.attr('string'),
     address: DS.attr('string'),
     phone:   DS.attr('string'),
-    books:   DS.hasMany('book'),
+
+    books: DS.hasMany('book', { inverse: 'library', async: true }),
 
     isValid: Ember.computed.notEmpty('name'),
     hasBody: Ember.computed.or('address', 'phone'),
+
+    randomize() {
+        this.set('name', Faker.company.companyName() + ' Library');
+        this.set('address', this._fullAddress());
+        this.set('phone', Faker.phone.phoneNumber());
+
+        // If you would like to use in chain.
+        return this;
+    },
+
+    _fullAddress() {
+        return `${Faker.address.streetAddress()}, ${Faker.address.city()}`;
+    }
 
 });
